3096eef0b09ad08286042117dfc0340480fe8cb2
SUTHARI AKSHAY SAI <123801262+Akshaysai04@users.noreply.github.com>
2025-07-24 12:09:37 +0530

    Time: 15 ms (17.05%), Space: 61.2 MB (5.53%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,7 +3,9 @@ class Solution {
         int n=nums.length;
         HashSet<Integer> a=new HashSet<>();
         for(int i:nums){
+        if(i<=nums.length && !a.contains(i) && i>0){
             a.add(i);
+        }
         }
         for(int i=1;i<=nums.length;i++){
             if(!a.contains(i)){
